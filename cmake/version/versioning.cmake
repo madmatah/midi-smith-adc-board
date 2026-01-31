df3169780c2f1b84310f73144e6a0e712158d6e9
Matthieu Huguet <matthieu@huguet.eu>
2026-01-31 17:02:18 +0100

    🐛 fix version header generator

--- cmake/version/versioning.cmake
+++ cmake/version/versioning.cmake
@@ -10,15 +10,24 @@ else()
   set(VERSION_BUILD_TYPE_ARG "${CMAKE_BUILD_TYPE}")
 endif()
 
-add_custom_target(
-  version_check ALL
+# We want to run the version script on every build to check if the git state changed.
+# To force Ninja/Make to always run the command, we make it produce a
+# non-existent file in addition to the header.
+add_custom_command(
+  OUTPUT "${VERSION_GENERATED_HEADER}" "FORCE_VERSION_CHECK"
   COMMAND "${CMAKE_COMMAND}"
-          -DOUTPUT_HEADER="${VERSION_GENERATED_HEADER}"
-          -DSOURCE_DIR="${CMAKE_SOURCE_DIR}"
-          -DVERSION_BUILD_TYPE="${VERSION_BUILD_TYPE_ARG}"
-          -P "${VERSION_SCRIPT}"
-  BYPRODUCTS "${VERSION_GENERATED_HEADER}"
-  DEPENDS "${VERSION_SCRIPT}"
+          -DOUTPUT_HEADER=${VERSION_GENERATED_HEADER}
+          -DSOURCE_DIR=${CMAKE_SOURCE_DIR}
+          -DVERSION_BUILD_TYPE=${VERSION_BUILD_TYPE_ARG}
+          -P ${VERSION_SCRIPT}
+  DEPENDS ${VERSION_SCRIPT}
+  COMMENT "Checking for firmware version updates..."
   VERBATIM
 )
 
+# This target is ALL, so it runs on every build.
+# It depends on the header (so the command runs).
+add_custom_target(
+  version_check ALL
+  DEPENDS "${VERSION_GENERATED_HEADER}"
+)
