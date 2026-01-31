4e5a93148ba3760e4f3e497f39af77a13934c622
Matthieu Huguet <matthieu@huguet.eu>
2026-01-31 17:29:12 +0100

    🗑️  cleanup unnecessary code used during prototyping

--- domain/include/domain/math.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace domain {
-
-int add(int a, int b) noexcept;
-
-}
--- domain/src/math.cpp
+++
@@ -1,9 +0,0 @@
-#include "domain/math.hpp"
-
-namespace domain {
-
-int add(int a, int b) noexcept {
-  return a + b;
-}
-
-}  // namespace domain
